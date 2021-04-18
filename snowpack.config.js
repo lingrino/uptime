ffbac8b40dc2dc8c96c2cf6eb2626e6287fca543
Sean Lingren <sean@lingrino.com>
2021-04-17 20:12:14 -0700

    add path support

--- snowpack.config.js
+++ snowpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
     public: { url: "/", static: true },
     src: { url: "/dist" },
   },
+  routes: [
+      {"match": "routes", "src": ".*", "dest": "/index.html"}
+  ],
   plugins: [
     "@snowpack/plugin-svelte",
     "@snowpack/plugin-dotenv",
--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,12 @@
 <script lang="typescript">
   import "./Styles.svelte";
-
   import Title from "./components/Title.svelte";
   import Input from "./components/Input.svelte";
   import Downtime from "./components/Downtime.svelte";
   import Footer from "./components/Footer.svelte";
+  import { pathNumber } from "./components/Math.svelte";
 
-  let uptime: number = 99.9;
+  let uptime = pathNumber(99.9);
 </script>
 
 <div class="flex flex-col min-h-screen bg-gray-100">
