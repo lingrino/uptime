0416c47f0191e23d2115804ba3de959eb19f34f3
Sean Lingren <sean@lingrino.com>
2021-05-22 22:15:07 -0700

    jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const forms = require("@tailwindcss/forms");
 
 module.exports = {
-  // mode: "jit", Enable after https://github.com/tailwindlabs/tailwindcss/issues/3950
+  mode: "jit",
   purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,svelte}"],
   plugins: [forms],
 };
