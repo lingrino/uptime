d35ffed4b95efd4e688a2d0bb6be1d7e1c5fb956
Sean Lingren <sean@lingrino.com>
2023-04-10 10:21:31 -0700

    add div to fix flashing content

--- src/routes/Uptime.svelte
+++ src/routes/Uptime.svelte
@@ -38,26 +38,26 @@
   }
 </script>
 
-<div>
-  <div class="mx-4 mb-10">
-    <div class="flex justify-center">
-      <div class="relative rounded-lg w-full max-w-md shadow-md">
-        <input
-          type="text"
-          class="py-3 pr-10 text-xl w-full rounded-lg {borderstyle}"
-          aria-label="uptime percentage"
-          bind:value={uptime}
-          on:input={handleInput}
-        />
-        <div
-          class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none"
-        >
-          <span class="text-gray-700 text-xl"> % </span>
-        </div>
+<div class="mx-4 mb-10">
+  <div class="flex justify-center">
+    <div class="relative rounded-lg w-full max-w-md shadow-md">
+      <input
+        type="text"
+        class="py-3 pr-10 text-xl w-full rounded-lg {borderstyle}"
+        aria-label="uptime percentage"
+        bind:value={uptime}
+        on:input={handleInput}
+      />
+      <div
+        class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none"
+      >
+        <span class="text-gray-700 text-xl"> % </span>
       </div>
     </div>
   </div>
-  <div class="flex justify-center">
+</div>
+<div class="flex justify-center">
+  <div>
     <table class="table-auto">
       {#each downtime as { name, value }}
         <div class="table-row">
