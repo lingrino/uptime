41f77a476b4d411ce9128f31b4ed4899c53d6365
Sean Lingren <sean@lingrino.com>
2024-03-10 13:42:41 -0700

    package upgrades

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from "@sveltejs/adapter-cloudflare";
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 const config = {
   preprocess: vitePreprocess(),
